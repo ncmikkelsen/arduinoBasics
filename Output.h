a95828e7b104ee4272e62d315af74af6987809f3
Nikolaj Mikkelsen <nikolaj.c.mikkelsen@gmail.com>
2016-04-20 12:46:58 +0200

    Initial commit with output class and todo in readme

---
+++ Output.h
@@ -0,0 +1,19 @@
+#ifndef Output_h
+#define Output_h
+
+#include "Arduino.h"
+
+class Output
+{
+  public:
+    Output(int pin);
+    void toggle();
+    bool getState();
+    void on();
+    void off();
+
+  private:
+    int _pin;
+    bool _state; 
+  };
+#endif
