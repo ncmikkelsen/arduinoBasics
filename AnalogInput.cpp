b073a6dbc9537e5f645547540e76d6f4f1f1a130
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2017-05-20 14:16:04 +0200

    Add shell for AnalogInput class

---
+++ AnalogInput.cpp
@@ -0,0 +1,2 @@
+#include "Arduino.h"
+#include "AnalogInput_h"
---
+++ AnalogInput.h
@@ -0,0 +1,15 @@
+#ifndef AnalogInput_h
+#define AnalogInput_h
+
+#include "Arduino.h"
+
+class AnalogInput
+{
+  public:
+
+
+  private:
+
+
+};
+#endif
