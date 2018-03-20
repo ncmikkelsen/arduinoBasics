a8962eb8a1c544c327671c7468b6196f65074fbe
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2018-03-20 12:33:44 +0100

    add functions and vars as outlined in the readme

--- AnalogInput.h
+++ AnalogInput.h
@@ -5,10 +5,31 @@
 
 class AnalogInput
 {
-  public:
+public:
+	AnalogInput(int pin);
+	AnalogInput(int pin, int threshhold);
+	void update();
+	int getValue();
+	void setThreshhold(int threshhold);
+	int getThreshhold();
+	bool isOver();
+	bool isUnder();
+	bool movedOver();
+	bool movedUnder();
+	bool stayedOver();
+	bool stayedUnder();
+	bool isOver(int threshhold);
+	bool isUnder(int threshhold);
+	bool movedOver(int threshhold);
+	bool movedUnder(int threshhold);
+	bool stayedOver(int threshhold);
+	bool stayedUnder(int threshhold);
 
-
-  private:
+private:
+	int _lastReading;
+	int _thisReading;
+	int _threshold;
+	int _pin;
 
 
 };
