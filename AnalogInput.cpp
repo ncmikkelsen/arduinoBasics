1d286bd9a4545f5c275162eed48cb1da58ae68f0
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2018-03-20 12:35:56 +0100

    add skeletons of functions

--- AnalogInput.cpp
+++ AnalogInput.cpp
@@ -1,2 +1,17 @@
 #include "Arduino.h"
 #include "AnalogInput_h"
+
+AnalogInput::AnalogInput(int pin){
+	_pin = pin;
+	_threshold = -1;
+	pinMode(_pin, INPUT);
+
+}
+
+AnalogInput::AnalogInput(int pin, int threshold){
+	_pin = pin;
+	_threshold = threshold;
+	pinMode(_pin, INPUT);
+}
+
+
