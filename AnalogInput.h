b3dd039aa4574136991b838c9eb8d0e9a2ef9731
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2018-03-25 16:34:25 +0200

    update indentation

--- AnalogInput.h
+++ AnalogInput.h
@@ -5,32 +5,30 @@
 
 class AnalogInput
 {
-public:
-	AnalogInput(int pin);
-	AnalogInput(int pin, int threshhold);
-	void update();
-	int getValue();
-	void setThreshhold(int threshhold);
-	int getThreshhold();
-	bool isOver();
-	bool isUnder();
-	bool movedOver();
-	bool movedUnder();
-	bool stayedOver();
-	bool stayedUnder();
-	bool isOver(int threshhold);
-	bool isUnder(int threshhold);
-	bool movedOver(int threshhold);
-	bool movedUnder(int threshhold);
-	bool stayedOver(int threshhold);
-	bool stayedUnder(int threshhold);
+	public:
+		AnalogInput(int pin);
+		AnalogInput(int pin, int threshold);
+		void update();
+		int getValue();
+		void setThreshhold(int threshold);
+		int getThreshhold();
+		bool isOver();
+		bool isUnder();
+		bool movedOver();
+		bool movedUnder();
+		bool stayedOver();
+		bool stayedUnder();
+		bool isOver(int threshold);
+		bool isUnder(int threshold);
+		bool movedOver(int threshold);
+		bool movedUnder(int threshold);
+		bool stayedOver(int threshold);
+		bool stayedUnder(int threshold);
 
-private:
-	int _lastReading;
-	int _thisReading;
-	int _threshold;
-	int _pin;
-
-
+	private:
+		int _lastReading;
+		int _thisReading;
+		int _threshold;
+		int _pin;
 };
 #endif
