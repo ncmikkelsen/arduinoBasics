c0c5fad4b1916f78779423e7cc395f82f05469a8
ncmikkelsen <nikolaj.c.mikkelsen@gmail.com>
2018-04-05 12:16:58 +0200

    fix by using proper function syntax

--- AnalogInput.cpp
+++ AnalogInput.cpp
@@ -17,72 +17,68 @@ AnalogInput::AnalogInput(int pin, int threshold){
 	pinMode(_pin, INPUT);
 }
 
-void update() {
+void AnalogInput::update() {
 	_lastReading = _thisReading;
 	_thisReading = analogRead(_pin);
 }
 
-int getValue(){
+int AnalogInput::getValue(){
 	return _thisReading;
 }
 
-void setThreshhold(int threshold){
+void AnalogInput::setThreshhold(int threshold){
 	_threshold = threshold;
 }
 
-int getThreshhold(){
+int AnalogInput::getThreshhold(){
 	return _threshold;
 }
 
-bool isOver(){
+bool AnalogInput::isOver(){
 	return _thisReading > _threshold;
 }
 
-bool isUnder(){
+bool AnalogInput::isUnder(){
 	return _thisReading < _threshold;
 }
 
-bool movedOver(){
+bool AnalogInput::movedOver(){
 	return (_thisReading > _threshold && _lastReading < _threshold);
 }
 
-bool movedUnder(){
+bool AnalogInput::movedUnder(){
 	return (_thisReading < _threshold && _lastReading > _threshold);
 }
 
-bool stayedOver(){
+bool AnalogInput::stayedOver(){
 	return (_thisReading > _threshold && _lastReading > _threshold);
 }
 
-bool stayedUnder(){
+bool AnalogInput::stayedUnder(){
 	return (_thisReading < _threshold && _lastReading < _threshold);
 }
 
-bool isOver(int threshold){
+bool AnalogInput::isOver(int threshold){
 	return _thisReading > threshold;
 }
 
-bool isUnder(int threshold){
+bool AnalogInput::isUnder(int threshold){
 	return _thisReading < threshold;
 }
 
-bool movedOver(int threshold){
+bool AnalogInput::movedOver(int threshold){
 	return (_thisReading > threshold && _lastReading < threshold);
 }
 
-bool movedUnder(int threshold){
+bool AnalogInput::movedUnder(int threshold){
 	return (_thisReading < threshold && _lastReading > threshold);
 }
 
-bool stayedOver(int threshold){
+bool AnalogInput::stayedOver(int threshold){
 	return (_thisReading > threshold && _lastReading > threshold);
 }
 
-bool stayedUnder(int threshold){
+bool AnalogInput::stayedUnder(int threshold){
 	return (_thisReading < threshold && _lastReading < threshold);
 }
-
-
-
-
 
